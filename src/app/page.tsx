d431b4000dc5860f307b6e965b7aab3d81be6a2d
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-08-07 21:26:52 -0300

    feat: add Header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
-import { Button } from './_components/ui/button'
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Teste</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Anderson</h2>
+        <p>Segunda-Feira, 05 de agosto</p>
 
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
