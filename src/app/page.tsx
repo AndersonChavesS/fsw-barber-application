b5f423ab36427fc55d791f7f309c4882d523233f
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-08-07 22:33:41 -0300

    feat: add booking item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar } from "./_components/ui/avatar"
+import { AvatarImage } from "@radix-ui/react-avatar"
 
 const Home = () => {
   return (
@@ -27,6 +31,28 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+        <h2 className="mt-6 text-xl uppercase">Agendamentos</h2>
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="size-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Vintage Barber</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">16:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
