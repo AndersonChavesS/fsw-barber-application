20d21376d6687b0ae87f6ff68afbdb463f97368e
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-08-12 17:57:28 -0300

    feat: update welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -45,8 +47,19 @@ const Home = async () => {
     <div>
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Anderson!</h2>
-        <p>Segunda-Feira, 05 de agosto</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "bem vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd ", { locale: ptBR })}
+          </span>
+          de
+          <span className="capitalize">
+            {" "}
+            {format(new Date(), " MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         <div className="mt-6">
           <Search />
