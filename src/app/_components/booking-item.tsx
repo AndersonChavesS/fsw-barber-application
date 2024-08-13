1a013340ca9c2572ab1574f74d3af1bc39cbc4c3
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-08-12 22:04:42 -0300

    chore: add NEXT_AUTH_SECRET env variable

--- src/app/_components/booking-item.tsx
+++ src/app/_components/booking-item.tsx
@@ -67,7 +67,7 @@ const BookingItem = ({ booking }: BookingItemProps) => {
   }
   return (
     <Sheet open={isSheetOpen} onOpenChange={handleSheetOpenChange}>
-      <SheetTrigger className="w-full">
+      <SheetTrigger className="w-full" asChild>
         <Card className="min-w-[90%]">
           <CardContent className="flex justify-between p-0">
             <div className="flex flex-col gap-2 py-5 pl-5">
